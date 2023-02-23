f2732d9916b8e3990efce41c0653bd142d66d67b
alena <evstafienko_a@ukr.net>
2023-02-23 09:17:26 +0200

    1

--- src/pages/Cast/Cast.js
+++ src/pages/Cast/Cast.js
@@ -1,34 +1,34 @@
-import { CastFilm } from "components/API";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+// import { CastFilm } from "components/API";
+// import { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
 
 const Cast = () => {
   console.log("cast");
-  const { id } = useParams();
-  const [infoAboutFilm, setInfoAboutFilm] = useState(null);
+  // const { id } = useParams();
+  // const [infoAboutFilm, setInfoAboutFilm] = useState(null);
 
-  useEffect(() => {
-    CastFilm(id)
-      .then(resolve => {
-        setInfoAboutFilm(resolve);
-        console.log(infoAboutFilm);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, [id]);
+//   useEffect(() => {
+//     CastFilm(id)
+//       .then(resolve => {
+//         setInfoAboutFilm(resolve);
+//         console.log(infoAboutFilm);
+//       })
+//       .catch(error => {
+//         console.log(error);
+//       });
+//   }, [id]);
 
   
-  return (
-    <ul>
-  <li>
-    <img />
-    <h2></h2>
-    <p>Character: { }</p>
-  </li>
+//   return (
+//     <ul>
+//   <li>
+//     <img />
+//     <h2></h2>
+//     <p>Character: { }</p>
+//   </li>
   
-</ul>
-  )
+// </ul>
+//   )
 }
 
 export default Cast;
